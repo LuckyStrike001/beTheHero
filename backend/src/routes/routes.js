79e21f2248c5fecf2898fddc9b9ce1b373b2e1f4
Bruno Novais <NB25372@novabase.pt>
2020-04-15 00:40:13 +0100

    delete route

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -8,6 +8,8 @@ const routes = express.Router();
 routes.get("/ongs", ongController.index);
 routes.post("/ongs", ongController.create);
 
+routes.get("/incidents", incidentController.index);
 routes.post("/incidents", incidentController.create);
+routes.delete("/incidents/:id", incidentController.delete);
 
 module.exports = routes;
