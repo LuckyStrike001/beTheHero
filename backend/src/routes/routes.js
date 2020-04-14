93ecdbd66ff16792a55c60dbc43cac11210071f3
Bruno Novais <NB25372@novabase.pt>
2020-04-14 01:24:23 +0100

    incidents creation

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,31 +1,13 @@
 const express = require("express");
-const crypto = require("crypto");
 
-const connection = require("../database/connection");
+const ongController = require("../controllers/OngController");
+const incidentController = require("../controllers/incidentController");
 
 const routes = express.Router();
 
-routes.get("/ongs", async (request, response) => {
-  const ongs = await connection("ongs").select("*");
+routes.get("/ongs", ongController.index);
+routes.post("/ongs", ongController.create);
 
-  return response.json(ongs);
-});
-
-routes.post("/ongs", async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body;
-
-  const id = crypto.randomBytes(4).toString("HEX");
-
-  await connection("ongs").insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  });
-
-  return response.json({ id });
-});
+routes.post("/incidents", incidentController.create);
 
 module.exports = routes;
