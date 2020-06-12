8705394e98eec2c407fc29ee0964c79c5ca39df7
Bruno Novais <bruno.br001@gmail.com>
2020-06-12 02:01:39 +0100

    New Incident v1

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./styles.css";
@@ -7,6 +7,10 @@ import { FiArrowLeft } from "react-icons/fi";
 import logoImg from "../../assets/logo.svg";
 
 export default function NewIncident() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -23,10 +27,21 @@ export default function NewIncident() {
           </Link>
         </section>
         <form>
-          <input placeholder="Case title" />
-          <textarea placeholder="Description" />
-
-          <input placeholder="Value" />
+          <input
+            placeholder="Case title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Value"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
 
           <button className="button" type="submit">
             Register
